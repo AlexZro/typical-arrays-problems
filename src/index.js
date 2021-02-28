4cd0f47b6af65ea10f5754f276829fcefb343259
Alex-Zr <alexzrodnikov@gmail.com>
2021-02-28 11:57:25 +0200

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+  if (array && array.length != 0) {
+    return Math.min(...array);
+  }
+
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length !== 0) {
+    return Math.max(...array);
+  }
+
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length !== 0) {
+    return array.reduce((sum, current) => sum + current, 0) / array.length;
+  }
+
   return 0;
 }
